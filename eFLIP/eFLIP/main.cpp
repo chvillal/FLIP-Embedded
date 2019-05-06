81d775544cf4eabcba82a717b22146b8227b9d46
Christopher Villalpando <chvillal@ucsc.edu>
2019-05-05 22:19:42 -0700

    added xcode project
    for building libraries and debugging and code

---
+++ eFLIP/eFLIP/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Unit Testing for eFLIP
+//
+//  Created by Christopher Villalpando on 5/5/19.
+//  Copyright © 2019 Christopher Villalpando. All rights reserved.
+//
+
+#include <iostream>
+#include "eflip.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ eFLIP/eflip.cpp
@@ -0,0 +1,9 @@
+//
+//  eflip.cpp
+//  eFLIP
+//
+//  Created by Christopher Villalpando on 5/5/19.
+//  Copyright © 2019 Christopher Villalpando. All rights reserved.
+//
+
+#include "eflip.hpp"
