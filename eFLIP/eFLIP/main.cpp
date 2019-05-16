df7c5ea005b8ef3ab892214f793428162cfb19a8
Christopher Villalpando <chvillal@ucsc.edu>
2019-05-16 16:17:29 -0700

    cleaned main

--- eFLIP/eFLIP/main.cpp
+++ eFLIP/eFLIP/main.cpp
@@ -10,54 +10,22 @@
 #include <bitset>
 #include "eflip.h"
 
+void test_metaheader(void);
+
 int main(int argc, const char * argv[]) {
     
-    FlipSocket mysocket;
+    test_metaheader();
+    
+//    mysocket.set_version(0);
+//    mysocket.set_dest(128);
+//    mysocket.set_type(4);
+//    mysocket.set_ttl(3);
+//    mysocket.set_flow(65535);
+//    mysocket.set_src(48);
+//    mysocket.set_len(16);
+//    mysocket.set_checksum(234);
+//    mysocket.set_offset(45);
 
-    //print_metaheader(mysocket);
-    std::cout << "\n\n";
-    mysocket.set_metaheader(FLIP_ESP, true);
-    mysocket.set_metaheader(FLIP_VERSION, true);
-    mysocket.set_metaheader(FLIP_DEST_1, true);
-    mysocket.set_metaheader(FLIP_TYPE, true);
-    mysocket.set_metaheader(FLIP_TTL, true);
-    mysocket.set_metaheader(FLIP_FLOW, true );
-//    mysocket.set_metaheader(FLIP_SOURCE_1, true );
-//    mysocket.set_metaheader(FLIP_LENGTH, true );
-//    mysocket.set_metaheader(FLIP_CHECKSUM, true );
-//    mysocket.set_metaheader(FLIP_NOFRAG, true );
-//    mysocket.set_metaheader(FLIP_LASTFRAG, true );
-    
-    mysocket.set_version(0);
-    mysocket.set_dest(128);
-    mysocket.set_type(4);
-    mysocket.set_ttl(3);
-    mysocket.set_flow(65535);
-    mysocket.set_src(48);
-    mysocket.set_len(16);
-    mysocket.set_checksum(234);
-    mysocket.set_offset(45);
-
-    //print_metafields(mysocket);
-    //print_metaheader(mysocket);
-    std::cout << "\n";
-    
-    mysocket.set_cont_bits();
-    
-    print_metaheader(mysocket);
-    std::cout << "\n";
-
-    uint8_t *a;
-    a = get_flip_metaheader(mysocket);
-    std::cout << std::hex << (int) *a << " " << (int) *(a+1) << " " <<  (int) *(a+2) << std::endl;
-    std::cout << std::string((char*) a) << std::string((char*) a+1) << std::string((char*) a+2) << std::endl;
-    std::cout << std::bitset<8> (*a) << " " << std::bitset<8> (*(a+1)) << " " << std::bitset<8> (*(a+2))<<  std::endl;
-
-    std::cout << "\n";
-    //print_metaheader(mysocket);
-    std::cout << "\n";
-    
-    
     
 //    //TEST bitwise operations and bitmap builder
 //    int x = 0x400000;
@@ -79,4 +47,39 @@ int main(int argc, const char * argv[]) {
     
     
     return 0;
+}
+
+void test_metaheader(void)
+{
+    FlipSocket mysocket;
+    
+    //print_metaheader(mysocket);
+    std::cout << "\n\n";
+    mysocket.set_metaheader(FLIP_ESP, true);
+    mysocket.set_metaheader(FLIP_VERSION, true);
+    mysocket.set_metaheader(FLIP_DEST_1, true);
+    mysocket.set_metaheader(FLIP_TYPE, true);
+    mysocket.set_metaheader(FLIP_TTL, true);
+    mysocket.set_metaheader(FLIP_FLOW, true );
+    //    mysocket.set_metaheader(FLIP_SOURCE_1, true );
+    //    mysocket.set_metaheader(FLIP_LENGTH, true );
+    //    mysocket.set_metaheader(FLIP_CHECKSUM, true );
+    //    mysocket.set_metaheader(FLIP_NOFRAG, true );
+    //    mysocket.set_metaheader(FLIP_LASTFRAG, true );
+
+    std::cout << "\n";
+    mysocket.set_cont_bits();
+    
+    print_metaheader(mysocket);
+    std::cout << "\n";
+    
+    uint8_t *a;
+    a = get_flip_metaheader(mysocket);
+    std::cout << std::hex << (int) *a << " " << (int) *(a+1) << " " <<  (int) *(a+2) << std::endl;
+    std::cout << std::string((char*) a) << std::string((char*) a+1) << std::string((char*) a+2) << std::endl;
+    std::cout << std::bitset<8> (*a) << " " << std::bitset<8> (*(a+1)) << " " << std::bitset<8> (*(a+2))<<  std::endl;
+    
+    std::cout << "\n";
+    //print_metaheader(mysocket);
+    std::cout << "\n";
 }
